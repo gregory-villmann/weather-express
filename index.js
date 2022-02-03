4f81ad45e3ac3fc8c7d862b49bf07e4bc62e2db9
Gregory Villmann <gregory.villmann@khk.ee>
2022-02-03 15:04:33 +0200

    Created API error handling

--- index.js
+++ index.js
@@ -2,30 +2,61 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const {render} = require("ejs");
+const {response} = require("express");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 let city = 'Tartu'
 
-app.get('/', function (req, res){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f68bed6ed513095148620e7453abb2e`)
-        .then((response)=>{
-            return response.json();
+const getWeatherData = (url) =>{
+    return new Promise((resolve, reject)=>{
+        fetch(url)
+            .then(response =>{
+                return response.json()
+            })
+            .then(data =>{
+                let result = {
+                    desc: data.weather[0].description,
+                    city: data.name,
+                    temp: Math.round(parseFloat(data.main.temp - 273.15)),
+                    error: null,
+                }
+                resolve(result)
+            })
+            .catch(error =>{
+                reject(error);
+            })
+    })
+}
+
+app.all('/', function (req, res){
+    let city;
+    if(req.method === "GET"){
+        city = "tartu";
+    }
+    if (req.method === "POST"){
+        city = req.body.cityname
+    }
+    let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f68bed6ed513095148620e7453abb2e`;
+    getWeatherData(url)
+        .then(data =>{
+            res.render('index', data);
         })
-        .then((data)=>{
-            let desc = data.weather[0].description;
-            let city = data.name;
-            let temp = Math.round(parseFloat(data.main.temp - 273.15));
-
-            res.render('index', {
-                desc: desc,
-                city: city,
-                temp: temp
-
-            });
+        .catch(error =>{
+            res.render('index', {error: "Oops! Something went wrong, try again"})
         })
-
-
 })
-app.listen(3069);
+
+app.get("*", function (req,res){
+    res.render('404')
+})
+
+app.listen(3069, ()=>{
+    console.log("Server started on http://localhost:3069");
+});
